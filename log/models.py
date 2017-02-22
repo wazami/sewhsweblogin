911d9934960ab6a5b54bf97c9f6218dba168ba10
wazami <hamzahanizami@gmail.com>
2017-02-21 22:07:29 -0500

    Changes Made From Reddit
    Implemented the suggestion from reddit to incorporate a club and user
    model in to the program. Also started creating the club_specific_view
    function so that each club has their own specific view. However, still
    confused on how to make it so that each club has their own view.

--- log/models.py
+++ log/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    #some stuff for a user model
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100) #this can have a function in which you authenticate
 
 class Club(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, related_name="owned_clubs")
     members = models.ManyToManyField(User, blank=True)
+
--- log/views.py
+++ log/views.py
@@ -2,11 +2,21 @@
 #log/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from log.models import Club
 
 # Create your views here.
 # this login required decorator is to not allow to any  
 # view without authenticating
 @login_required(login_url="login/")
 def home(request):
-	return render(request,"home.html")
+    return render(request,"home.html")
 
+def club_specific_view(request):
+    #I should have a club id associated
+    club = Club.name
+    if club.owner is not request.user:
+        print('NOPE!')
+        #redirect this dude to an HTML page saying error?
+        return render(request, "error_page.html")
+	else:
+		return render(request, #html file with the correct view)
